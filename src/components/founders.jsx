c5fd3133c98981facd7a492b000a03ada94375ec
Abhishek Gobind Rao <101335115+imAbhishekRao@users.noreply.github.com>
2024-07-02 22:44:03 +0530

    Update founders.jsx

--- src/components/founders.jsx
+++ src/components/founders.jsx
@@ -13,11 +13,11 @@ export default function Brands() {
     <div className="full-screen-svg">
       <img src={fullScreenSVG2} alt="Full Screen SVG" useMap="#image-map" className="svg-image" />
       <map name="image-map">
-        <Link to={'/brands'}>
+        <Link to={'/'}>
         <area
           alt="Clickable Area Brands"
           title="Clickable Area Brands"
-          coords="47px,500px,525px,511px"
+          coords="27px,108px,390px,280px"
           shape="rect"
           onClick={handleAreaClick}
           className="clickable-area"
